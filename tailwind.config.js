d704f4c819424dddbcf9308ff14f49b3b3efedee
حسان ونس <36311551+Hassan-Wanas@users.noreply.github.com>
2023-03-01 08:35:13 +0200

    add tailwindcss & style mobile version

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        White1: '#A9A9A9',
+        White2: '#C4C4C4',
+      }
+    },
+    fontFamily: {
+      OpenSans: 'Open Sans',
+      PTSerif: 'PT Serif',
+      Poppins: 'Poppins',
+      Montserrat: 'Montserrat',
+    },
+  },
+  plugins: [],
+}
